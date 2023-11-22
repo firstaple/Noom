190b3ecf31089c1d1e457fed95737d0352aa5097
firstaple <firstaple@gmail.com>
2023-11-22 10:55:03 +0900

    Nicknames part Two

--- src/server.js
+++ src/server.js
@@ -23,11 +23,22 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
   socket.on("close", () => onSocketClose);
-  socket.on("message", (message) => {
-    message = message.toString("utf-8");
-    sockets.forEach((aSocket) => aSocket.send(message));
+  socket.on("message", (msg) => {
+    msg = msg.toString("utf-8");
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
